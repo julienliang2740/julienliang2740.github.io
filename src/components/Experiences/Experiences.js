1871fe2f0d9eaeddb6057f2997cb22e0fc208857
Julien Liang <julienliang2740@gmail.com>
2024-08-01 11:04:17 -0400

    backup to fall back onto, timeline component

--- src/components/Experiences/Experiences.js
+++ src/components/Experiences/Experiences.js
@@ -1,25 +1,3 @@
-// import { myExperiences } from '../../portfolio'
-// import './Experiences.css'
-
-// const Experiences = () => {
-//   const {role, description} = myExperiences
-//   if (!Experiences) return null
-//   return (
-//     <div className='experiences center'>
-
-//       {role && <h2 className='experiences__role' id='experiences'>A {role}.</h2>}
-//       <p className='experiences__desc'>{description && description}</p>
-
-//       <div className='experiences__contact center'/>
-
-//     </div>
-    
-//   )
-// }
-
-// export default Experiences
-
-
 // Experiences.js
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
@@ -27,6 +5,7 @@ import 'react-vertical-timeline-component/style.min.css';
 import { FaBriefcase } from 'react-icons/fa';
 import { myExperiences } from '../../portfolio';
 import './Experiences.css';
+// import './TimelinComponent.css';
 
 const Experiences = () => {
   if (!myExperiences || myExperiences.length === 0) return null;
@@ -38,15 +17,16 @@ const Experiences = () => {
         {myExperiences.map((experience, index) => (
           <VerticalTimelineElement
             key={experience.id || index}
-            // className="vertical-timeline-element--work"
-            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+            className="vertical-timeline-element--work"
+            contentStyle={{color: 'var(--clr-fg)', background:'rgba(0,0,0,0)'}}
+            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)', background:'rgba(0,0,0,0)'}}
             date={experience.date}
-            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            iconStyle={{ background: 'var(--clr-primary)', color: '#fff' }}
             icon={<FaBriefcase />}
+            style={{color: 'var(--clr-fg)'}}
           >
-            <h3 className="vertical-timeline-element-title">{experience.role}</h3>
-            <h4 className="vertical-timeline-element-subtitle">{experience.company}</h4>
+            <h3 className="experiences__timeline__role">{experience.role}</h3>
+            <h4 className="experiences__timeline__company">{experience.company}</h4>
             <p>{experience.description}</p>
           </VerticalTimelineElement>
         ))}
