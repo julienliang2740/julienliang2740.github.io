e1ea7818f3d7b538fe133397afc7b717541a5465
Julien Liang <julienliang2740@gmail.com>
2024-08-01 12:27:43 -0400

    milestone

--- src/components/Experiences/Experiences.js
+++ src/components/Experiences/Experiences.js
@@ -15,21 +15,19 @@ const Experiences = () => {
       <h2 className='experiences__header' id='experiences'>My Experiences</h2>
       <VerticalTimeline>
         {myExperiences.map((experience, index) => (
-          <VerticalTimelineElement
+          <VerticalTimelineElement 
             key={experience.id || index}
             className="vertical-timeline-element--work"
-            contentStyle={{color: 'var(--clr-fg)', background:'rgba(0,0,0,0)'}}
-            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)', background:'rgba(0,0,0,0)'}}
             date={experience.date}
-            iconStyle={{ background: 'var(--clr-primary)', color: '#fff' }}
             icon={<FaBriefcase />}
-            style={{color: 'var(--clr-fg)'}}
           >
             <h3 className="experiences__timeline__role">{experience.role}</h3>
             <h4 className="experiences__timeline__company">{experience.company}</h4>
             <p>{experience.description}</p>
           </VerticalTimelineElement>
         ))}
+    <VerticalTimelineElement
+      />
       </VerticalTimeline>
     </div>
   );
