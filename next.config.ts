4f980c9435b9d4db704cf4a29c8cbb5e0233cd41
julien <julienliang2740@gmail.com>
2026-04-20 16:33:48 -0400

    add deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: "julienliang2740.github.io",
   turbopack: {
     root: process.cwd(),
   },
